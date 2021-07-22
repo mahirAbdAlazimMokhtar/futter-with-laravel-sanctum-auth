027206278ac6dde0f3cb51cebb5e6fd8efde94c7
mahirabdalazimmokhtar <mahr.bwb@gmial.com>
2021-07-22 08:57:35 +0200

    add post screen

---
+++ lib/screens/post_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class PostScreen extends StatefulWidget {
+  const PostScreen({Key? key}) : super(key: key);
+
+  @override
+  _PostScreenState createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+      body: Center(child: Text('posts'),),
+    );
+  }
+}
