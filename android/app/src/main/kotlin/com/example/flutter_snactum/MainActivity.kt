091bd86e3f58d3605a30dadbf513995e757751a4
mahirabdalazimmokhtar <mahr.bwb@gmial.com>
2021-07-22 08:47:28 +0200

    project setup inti

---
+++ android/app/src/main/kotlin/com/example/flutter_snactum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_snactum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widget/nav_drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            title: Text('Mahir Abd Alazim'),
+          )
+        ],
+      )
+    );
+  }
+}
